cf3420d150d38ff9effd8f5cd1ba5a822ec07802
Unknown <thibaut.weissgerber@gmail.com>
2017-08-26 01:42:49 +0200

    _loopbackViewController
    Backup of working algorithm

--- Alvin/_LoopbackViewController.swift
+++ Alvin/_LoopbackViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 import SCSiriWaveformView
 
-class LoopbackViewController: UIViewController, AVAudioPlayerDelegate
+class _LoopbackViewController: UIViewController, AVAudioPlayerDelegate
 {
     let documentDirectory: URL = FileManager.shared.documentsDirectory!
     let recordingSettings = [
